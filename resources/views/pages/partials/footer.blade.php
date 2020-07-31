1b8c5f0e80c58013a7d167b4bb61ce6ac01522e5
Giosue1994 <giosuelentini1994@outlook.com>
2020-07-31 16:49:11 +0200

    footer

--- resources/views/pages/partials/footer.blade.php
+++ resources/views/pages/partials/footer.blade.php
@@ -1,10 +1,9 @@
 <!-- FOOTER -->
-  <div class="container">
+  <div class="container text-center">
     <div class="row">
-
       <div class="col-6">
         <img src="{{ asset('img/logo-white.png') }}" alt="Logo">
-        <ul>
+        <ul class="address text-left">
           <li>Sede Operativa</li>
           <li>Via Carducci 12 - 20123 Milano</li>
           <li>Tel: 02-40031288</li>
@@ -12,13 +11,21 @@
       </div>
 
       <div class="col-6">
-        <ul>
+        <ul class="link text-right">
           <li><a href="#">Lezione gratuita</a></li>
           <li><a href="{{ route('faq') }}">Domande frequenti</a></li>
           <li><a href="{{ route('privacy') }}">Privacy policy</a></li>
           <li><a href="#">Lavora con noi</a></li>
         </ul>
       </div>
-
     </div>
+
+      <ul class="social">
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+      </ul>
+
+      <p>Boolean SRL - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967</p>
   </div>
